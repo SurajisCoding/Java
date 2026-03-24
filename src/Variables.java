7a7d004ce40d07f791f66cb6de5a1490d11cfee8
SurajCodesPerfectly <ritikapankaj9@gmail.com>
2026-03-24 08:31:53 +0000

    I am learning Java

---
+++ src/Variables.java
@@ -0,0 +1,45 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        // Primitive Data Types
+
+        // Integer data type (holds whole numbers and negative numbers)
+        int age = 21;
+        int quantity = 2;
+
+        System.out.println(quantity);
+        System.out.println(age);
+        
+        // Double data type (Holds decimal point numbers)
+        double temperature = 3.4;
+        double gpa = 3.5;
+
+        System.out.println(temperature);
+        System.out.println(gpa);
+
+        // Char data type
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        System.out.println(grade);
+        System.out.println(symbol);
+        System.out.println(currency);
+
+        // boolean data type
+
+        boolean isStudent = true;
+        boolean isTeacher = false;
+        boolean isWorthy = true;
+        System.out.println(isStudent);
+        System.out.println(isTeacher);
+        System.out.println(isWorthy);
+
+        // Reference Data Types
+
+        // String Data type (holds a series of characters(char))
+
+        String name = "";
+    }
+}
